4aa1c0930b69d7bb479bf355794d81ca400d2035
Walter Low <walterlow88@gmail.com>
2021-08-08 01:35:55 +0800

    Added spdlog submodule and Logging system.

---
+++ Hazel/src/Hazel/Log.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Hazel
+{
+	class HAZEL_API Log
+	{
+	public: 
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLoger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+
+}
+
+// Core log macros
+#define HZ_CORE_TRACE(...)    ::Hazel::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define HZ_CORE_INFO(...)     ::Hazel::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define HZ_CORE_WARN(...)     ::Hazel::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define HZ_CORE_ERROR(...)    ::Hazel::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define HZ_CORE_CRITICAL(...) ::Hazel::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+// Client log macros
+#define HZ_TRACE(...)         ::Hazel::Log::GetClientLoger()->trace(__VA_ARGS__)
+#define HZ_INFO(...)          ::Hazel::Log::GetClientLoger()->info(__VA_ARGS__)
+#define HZ_WARN(...)          ::Hazel::Log::GetClientLoger()->warn(__VA_ARGS__)
+#define HZ_ERROR(...)         ::Hazel::Log::GetClientLoger()->error(__VA_ARGS__)
+#define HZ_CRITICAL(...)      ::Hazel::Log::GetClientLoger()->critical(__VA_ARGS__)
